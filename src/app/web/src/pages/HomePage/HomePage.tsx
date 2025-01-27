ef3418af3774ea1fbd28ac7817479ad45016aa70
Mike Seese <seesemichaelj@gmail.com>
2025-01-26 21:04:21 -0800

    get auth0 hooked up to app

---
+++ src/app/web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,17 @@
+import { useAuth } from 'src/auth'
+
+const HomePage = () => {
+  const { isAuthenticated, signUp, logIn } = useAuth()
+
+  return (
+    <>
+      {/* MetaTags, h1, paragraphs, etc. */}
+
+      <p>{JSON.stringify({ isAuthenticated })}</p>
+      <button onClick={logIn}>log in</button>
+      <button onClick={signUp}>sign up</button>
+    </>
+  )
+}
+
+export default HomePage
