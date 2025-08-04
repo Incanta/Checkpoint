fc967070582dc9072232124d821106926cbe84a8
Mike Seese <seesemichaelj@gmail.com>
2025-08-04 15:16:23 -0700

    add @incanta/config dependency for various config and env

--- src/app-new/src/server/auth/config.ts
+++ src/app-new/src/server/auth/config.ts
@@ -1,8 +1,17 @@
+import config from "@incanta/config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import Auth0Provider from "next-auth/providers/auth0";
+import GitHubProvider from "next-auth/providers/github";
+import GitLabProvider from "next-auth/providers/gitlab";
+import OktaProvider from "next-auth/providers/okta";
+import SlackProvider from "next-auth/providers/slack";
 import DiscordProvider from "next-auth/providers/discord";
 
 import { db } from "~/server/db";
+import { computePasswordHash } from "./credentials";
+import type { Provider } from "next-auth/providers";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -25,24 +34,102 @@ declare module "next-auth" {
   // }
 }
 
+const credentialsProvider = CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await db.user.findUnique({
+          where: { username: credentials?.username as string | undefined ?? "" },
+        });
+
+        if (user?.salt && user.hash) {
+          const hash = await computePasswordHash(
+            credentials?.password as string | undefined ?? "",
+            user.salt as string
+          );
+
+          if (hash === user.hash) {
+            return user;
+          }
+        }
+
+        return null;
+      },
+    });
+
+const auth0Provider = Auth0Provider({
+  clientId: config.get<string>("auth.auth0.client.id"),
+  clientSecret: config.get<string>("auth.auth0.client.secret"),
+  issuer: config.get<string>("auth.auth0.issuer"),
+});
+
+const githubProvider = GitHubProvider({
+  clientId: config.get<string>("auth.github.client.id"),
+  clientSecret: config.get<string>("auth.github.client.secret"),
+});
+
+const gitlabProvider = GitLabProvider({
+  clientId: config.get<string>("auth.gitlab.client.id"),
+  clientSecret: config.get<string>("auth.gitlab.client.secret"),
+});
+
+const oktaProvider = OktaProvider({
+  clientId: config.get<string>("auth.okta.client.id"),
+  clientSecret: config.get<string>("auth.okta.client.secret"),
+  issuer: config.get<string>("auth.okta.issuer"),
+});
+
+const slackProvider = SlackProvider({
+  clientId: config.get<string>("auth.slack.client.id"),
+  clientSecret: config.get<string>("auth.slack.client.secret"),
+});
+
+const discordProvider = DiscordProvider({
+  clientId: config.get<string>("auth.discord.client.id"),
+  clientSecret: config.get<string>("auth.discord.client.secret"),
+});
+
+const providers: Provider[] = [];
+
+if (config.get<boolean>("auth.credentials.enabled")) {
+  providers.push(credentialsProvider);
+}
+
+if (config.get<boolean>("auth.auth0.enabled")) {
+  providers.push(auth0Provider);
+}
+
+if (config.get<boolean>("auth.github.enabled")) {
+  providers.push(githubProvider);
+}
+
+if (config.get<boolean>("auth.gitlab.enabled")) {
+  providers.push(gitlabProvider);
+}
+
+if (config.get<boolean>("auth.okta.enabled")) {
+  providers.push(oktaProvider);
+}
+
+if (config.get<boolean>("auth.slack.enabled")) {
+  providers.push(slackProvider);
+}
+
+if (config.get<boolean>("auth.discord.enabled")) {
+  providers.push(discordProvider);
+}
+
 /**
  * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
  *
  * @see https://next-auth.js.org/configuration/options
  */
 export const authConfig = {
-  providers: [
-    DiscordProvider,
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
+  secret: config.get<string>("auth.jwt.secret"),
+  providers,
   adapter: PrismaAdapter(db),
   callbacks: {
     session: ({ session, user }) => ({
