fd1d4e169575fa8623c51f8ba71c455b9ed625b8
Mike Seese <seesemichaelj@gmail.com>
2026-06-22 18:16:39 -0700

    bump version to 0.3.4

--- src/clients/tray/version.go
+++ src/clients/tray/version.go
@@ -3,7 +3,7 @@
 package main
 
 // User-facing semver version of the bundled client installation.
-const ClientVersion = "0.3.3"
+const ClientVersion = "0.3.4"
 
 // Integer daemon-API version this tray was built against. Compared against
 // the daemon's min_daemon_api at connect time.
